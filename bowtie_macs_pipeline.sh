4bbbeb16b6ea3b62c916928c5aaf507a11920f17
Saket Choudhary <saketkc@gmail.com>
2014-10-22 07:32:57 -0700

    Generalise

--- bowtie_macs_pipeline.sh
+++ bowtie_macs_pipeline.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-bowtie -p 4 -M 1 -q -v 2 -k 1 -X 2000 --best --strata --sam GRCh37.fa -1 SRR891275_1.fastq -2 SRR891275_2.fastq GSM1155964_bowtie.sam
-samtools sort  GSM1155964_bowtie.bam GSM1155964_bowtie.sorted
-sambamba index -t 4 GSM1155964_bowtie.sorted.bam GSM1155964_bowtie.sorted.bam.bai
-bedtools bamtobed GSM1155964_bowtie.sorted.bam > GSM1155964_bowtie-ready.bed
-macs2 callpeak -t GSM1155964_bowtie-ready.bed --nomodel --nolambda --extsize 75 --SPMR -g hs -n rep1
+bowtie -p 4 -M 1 -q -v 2 -k 1 -X 2000 --best --strata --sam /usr/local/share/bcbio/genomes/Hsapiens/hg19/bowtie/hg19 -1 $1 -2 $2 "$3.sam"
+samtools view -@ 4 -h -S -b "$3.sam" -o "$3.bam"
+samtools sort "$3.bam"  "$3.sorted"
+sambamba index -t 4 "$3.sorted.bam" "$3.sorted.bam.bai"
+bedtools bamtobed -i "$3.sorted.bam" > "$3.sorted.bed"
+macs2 callpeak -t "$3.sorted.bed" --nomodel --nolambda --extsize 75 --SPMR -g hs -n "$3.macs2"

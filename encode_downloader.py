0cde5eda6147a24686234f6c9ad3536fdd5a763d
Saket Choudhary <saketkc@gmail.com>
2015-05-28 14:17:33 -0700

    Encode Downloader: Allow auto downloading controls

--- encode_downloader.py
+++ encode_downloader.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+"""
+Encode data downloader(Only BAMs are downloaded)
+"""
 import requests
-#import argparse
+import json
 import subprocess
 import sys
 import os
@@ -8,30 +11,71 @@ import os
 __base_url__ = 'https://www.encodeproject.org/'
 download_prefix = "/media/data1/ENCODE"
 
-def encode_downloader(args):
-    encode_id = args[0]
+def parser(encode_id):
     req = requests.get("https://www.encodeproject.org/experiments/{}/?format=json".format(encode_id))
-    json = req.json()
-    files = json['files']
-    ## Metadata format: {technical_replicate_number: {biological_replicate_number1: url, biological_replicate_number2: url}}
+    response_json = req.json()
+    biosample = response_json['biosample_term_name']
+    label = response_json['target']['label']
+    files = response_json['files']
+    replicates = len(response_json['replicates'])
+    controls = response_json['possible_controls']
     metadata = {}
     for f in files:
         href = f['href']
         if f['file_type']== 'bam':
-            bio_repl = f['replicate']['biological_replicate_number']
-            tech_repl = f['replicate']['technical_replicate_number']
+            if replicates == 1:
+                bio_repl = 1
+                tech_repl = 1
+            else:
+                bio_repl = f['replicate']['biological_replicate_number']
+                tech_repl = f['replicate']['technical_replicate_number']
             if tech_repl not in metadata.keys():
                 metadata[tech_repl] = {}
             if bio_repl not in metadata[tech_repl].keys():
                 metadata[tech_repl][bio_repl] = {}
-            metadata[tech_repl][bio_repl] = href
+            metadata[tech_repl][bio_repl] = {'href': href, 'metadata':f}
+    return {"replicates": replicates, "controls": controls, "label": label, "files": files, "biosample":biosample, "metadata":metadata}
+
+def downloader(files, metadata, path):
     for tech_repl in metadata.keys():
         for bio_repl in metadata[tech_repl].keys():
-            print bio_repl
-            href = __base_url__ + metadata[tech_repl][bio_repl]
-            location = os.path.join(download_prefix, encode_id, "tech_repl_{}".format(tech_repl), "bio_repl_{}".format(bio_repl))
+            href = __base_url__ + metadata[tech_repl][bio_repl]['href']
+            mdata = metadata[tech_repl][bio_repl]['metadata']
+            location = os.path.join(path, "tech_repl_{}".format(tech_repl), "bio_repl_{}".format(bio_repl))
             os.makedirs(location)
-            p = subprocess.call(["wget", "-c", href, "-P", location])
+            with open(os.path.join(location, "metadata.json"), 'w') as w:
+                json.dump(mdata, w)
+            subprocess.call(["wget", "-c", href, "-P", location])
 
+
+def control_downloader(controls=[], TF_data_path=""):
+    print len(controls)
+    for control in controls:
+        encode_id = control['accession']
+        print "Downloading control: ", encode_id
+        parsed = parser(encode_id)
+        biosample = parsed['biosample']
+        label = parsed['label']
+        metadata = parsed['metadata']
+        files = parsed['files']
+        job_name = "{}_{}_{}".format(biosample, label, encode_id)
+        downloader(files, metadata, os.path.join(TF_data_path, job_name))
+
+
+
+def encode_downloader(args):
+    encode_id = args[0]
+    parsed = parser(encode_id)
+    biosample = parsed['biosample']
+    label = parsed['label']
+    controls = parsed['controls']
+    replicates = parsed['replicates']
+    files = parsed['files']
+    job_name = "{}_{}_{}".format(biosample, label, encode_id)
+    metadata = parsed['metadata']
+    path = os.path.join(download_prefix, job_name)
+    print "Downloading controls"#, controls
+    control_downloader(controls, path)
+    downloader(files, metadata, path)
 if __name__=="__main__":
     encode_downloader(sys.argv[1:])

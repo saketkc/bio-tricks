8547b18f6e9616c717b885557831a144b65cd1cf
Saket Choudhary <saketkc@gmail.com>
2015-05-31 15:31:12 -0700

    Add meme summariser

---
+++ meme_parser/summarisememe.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from Bio import motifs
+import sys
+
+
+def main(argv):
+    handle = open(argv[0], 'r')
+    records = motifs.parse(handle, 'meme')
+    print "Total motifs present: {}".format(len(records))
+    for i, record in enumerate(records):
+        print "Motif {} \t Length: {}".format(i, len(record.consensus))
+if __name__ == '__main__':
+    main(sys.argv[1:])

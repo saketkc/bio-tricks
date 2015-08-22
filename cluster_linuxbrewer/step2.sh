401a0688699100d5a002513607377727054b49da
Saket Choudhary <saketkc@gmail.com>
2015-08-22 16:59:28 -0700

    Fix

--- cluster_linuxbrewer/step2.sh
+++ cluster_linuxbrewer/step2.sh
@@ -14,6 +14,6 @@ brew install libxml2 python
 brew install bzip2 coreutils findutils gawk gnu-sed gnu-which grep libpng libxml2 libxslt make ncurses readline ruby
 
 ##Ncurses hacks
-ln -s $HOMEBREW_HOME/Cellar/ncurses/5.9/include/nncursesw/curses.h ncursesw/form.h ncursesw/ncurses.h ncursesw/term.h ncursesw/termcap.h $HOMEBREW_H$
+ln -s $HOMEBREW_HOME/Cellar/ncurses/5.9/include/nncursesw/curses.h ncursesw/form.h ncursesw/ncurses.h ncursesw/term.h ncursesw/termcap.h $HOMEBREW_HOME/include/
 ln -s $HOMEBREW_HOME/Cellar/ncurses/5.9/lib/libncurses.so $HOMEBREW_HOME/lib/libcurses.so
 ln -s $HOMEBREW_HOME/Cellar/ncurses/5.9/lib/libncurses.a $HOMEBREW_HOME/lib/libcurses.a

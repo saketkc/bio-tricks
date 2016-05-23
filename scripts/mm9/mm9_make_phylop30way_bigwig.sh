740d7695e9b35dff06319dbb47b659fd5f698294
Saket Choudhary <saketkc@gmail.com>
2016-05-22 18:15:30 -0700

    Update

--- scripts/mm9/mm9_make_phylop30way_bigwig.sh
+++ scripts/mm9/mm9_make_phylop30way_bigwig.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-## Make mm9 Phylop30Way bigwig
 set -euo pipefail
 rsync -avz --progress rsync://hgdownload.cse.ucsc.edu/goldenPath/mm9/phyloP30way/vertebrate/* ./
-gunzip *.gz
-mkdir random
+gunzip -f *.gz
+mkdir -p random
 mv *_random.wigFix random
 cat *.wigFix > mm9.30way.phyloP.wig
 wigToBigWig mm9.30way.phyloP.wig ../fasta/mm9.chrom.sizes mm9.30way.phyloP.bw

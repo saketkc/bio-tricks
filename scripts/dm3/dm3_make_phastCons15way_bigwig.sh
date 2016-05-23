58b927b28755fad5c16506784b4a63dbdde60a03
Saket Choudhary <saketkc@gmail.com>
2016-05-23 09:28:00 -0700

    Fixes for dm3

--- scripts/dm3/dm3_make_phastCons15way_bigwig.sh
+++ scripts/dm3/dm3_make_phastCons15way_bigwig.sh
@@ -3,5 +3,28 @@
 set -euo pipefail
 rsync -avz --progress rsync://hgdownload.cse.ucsc.edu/goldenPath/dm3/phastCons15way/ ./
 gunzip -f *.gz
+
+## Select one value for chr2R since there is an overlap
+## Otherwise causes this:
+## There's more than one value for chr2R base 5000001 (in coordinates that start with 1).
+
+sed -i.orig -e '4963003d' chr2R.pp
+
+## Fix for:
+## There's more than one value for chr3L base 7000001 (in coordinates that start with 1).
+
+sed -i.orig -e '6969685d' chr3R.pp
+
+
+## Fix for:
+## There's more than one value for chrX base 12000001 (in coordinates that start with 1).
+
+sed -i.orig -e '11943831d' chrX.pp
+
+## Fix for:
+## There's more than one value for chr3L base 7000001 (in coordinates that start with 1).
+
+sed -i.orig -e '6993258d;10989627d' chr3L.pp
+
 cat *.pp > dm3.15way.phastCons.wig
 wigToBigWig dm3.15way.phastCons.wig ../fasta/dm3.chrom.sizes dm3.15way.phastCons.bw
